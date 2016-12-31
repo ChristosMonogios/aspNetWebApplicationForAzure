2631a7e99fa8dc2b9fbe7039268c72bc11fcac57
Chris Monogios <chr_mon_@hotmail.com>
2016-12-31 10:55:13 +0100

    Add project files.

---
+++ aspNetWebApplicationForAzure/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(aspNetWebApplicationForAzure.Startup))]
+namespace aspNetWebApplicationForAzure
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
